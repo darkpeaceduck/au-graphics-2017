dc130010ae8673c365d8c7e47b7492d1044b87e4
darkpeaceduck <okay123456@yandex.ru>
2018-01-25 14:41:50 +0300

    fix dragging

--- 1/src/mbrot.cpp
+++ 1/src/mbrot.cpp
@@ -157,16 +157,34 @@ void bn_handler(int bn, int state, int x, int y) {
 	}
 }
 
+void mouse_handler2(int bn, int state, int x, int y) {
+    int xres = glutGet(GLUT_WINDOW_WIDTH);
+    int yres = glutGet(GLUT_WINDOW_HEIGHT);
+    float fx = 2.0 * ((float)x / (float)xres - 0.5);
+    float fy = 2.0 * ((float)y / (float)yres - 0.5);
+
+    if(which_bn == 1) {
+        cx += (fx - px) * scale / 2.0;
+        cy -= (fy - py) * scale / 2.0;
+    } else if(which_bn == 0) {
+        scale *= (fy - py < 0.0) ? 1 - zoom_factor : 1 + zoom_factor;
+    }
+
+    px = fx;
+    py = fy;
+    draw();
+}
+
 void mouse_handler(int x, int y) {
 	int xres = glutGet(GLUT_WINDOW_WIDTH);
 	int yres = glutGet(GLUT_WINDOW_HEIGHT);
 	float fx = 2.0 * ((float)x / (float)xres - 0.5);
 	float fy = 2.0 * ((float)y / (float)yres - 0.5);
 
-	if(which_bn == 1) {
+	if(which_bn == 0) {
 		cx += (fx - px) * scale / 2.0;
 		cy -= (fy - py) * scale / 2.0;
-	} else if(which_bn == 0) {
+	} else if(which_bn == 1) {
 		scale *= (fy - py < 0.0) ? 1 - zoom_factor : 1 + zoom_factor;
 	}
 
